a8075e263b889633447d83ab7a13dea2eacd45a5
David Lee <namesdavidlee@gmail.com>
2018-07-15 12:39:30 -0700

    clean up

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import { render } from 'react-dom';
 import App from './containers/App';
 import registerServiceWorker from './registerServiceWorker';
 
-// import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 
 render(<App />, document.getElementById('root'));
